6b7cd7b76683de2a2bd2fc4c924d0a664ef21990
Aki Sasaki <asasaki@mozilla.com>
2021-11-09 16:48:09 +0000

    no bug - update pinned requirements, add pin.sh. r=ahal,releng-reviewers
    Mainly to standardize pinning, plus take the `tc-admin diff` speedup from https://github.com/taskcluster/tc-admin/pull/195
    
    Also unsafe-pin, because pip-tools 6.4.0 from pip-compile-multi 2.4.2 requires pip>=21.2 without hashes, which breaks including hashes in test.txt unless we include pip.
    
    This also bumps us to py39 to match cloudops-jenkins.
    
    Differential Revision: https://phabricator.services.mozilla.com/D130486

---
+++ maintenance/pin-helper.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# This runs in docker to pin our requirements files.
+set -e
+SUFFIX=${SUFFIX:-txt}
+
+pip install --upgrade pip
+pip install pip-compile-multi
+
+# XXX pip-tools 6.4.0 from pip-compile-multi 2.4.2 requires
+#     pip>=21.2 without hashes, which breaks including hashes in test.txt >_<
+#     add -s to allow for "unsafe" pinning, i.e. include `pip`
+ARGS="-g base -g test -g local -s"
+pip-compile-multi -o "$SUFFIX" $ARGS
+chmod 644 requirements/*.txt
---
+++ maintenance/pin.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# Pin all requirements.
+#
+# As of this writing, cloudops-jenkins uses an image based off of python:3.9
+# to run ci-admin
+#
+# To test, after running maintenance/pin.sh:
+#
+#    docker pull gcr.io/moz-fx-cloudops-images-global/cloudops-infra-deploylib:2
+#    docker run -it --mount type=bind,source="$(pwd)",target=/mnt gcr.io/moz-fx-cloudops-images-global/cloudops-infra-deploylib:2  bash
+#    python -mvenv x
+#    . x/bin/activate
+#    pip install -r /mnt/requirements/local.txt
+
+
+set -e
+set -x
+
+docker run -t -v $PWD:/src -w /src python:3.9 maintenance/pin-helper.sh
