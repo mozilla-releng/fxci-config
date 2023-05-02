678de9f103854a2087a517c7e7a7f69334d6168b
Andrew Halberstadt <ahal@mozilla.com>
2023-05-02 13:06:27 +0000

    Bug 1830414 - Add a check to detect some insecure grants, r=jcristau,releng-reviewers,bhearsum
    This check is best effort and not comprehensive. It may also result in false
    positives. That said, after experimenting with a bunch of different ways to add
    level 3 scopes to level 1 contexts, it appears to work surprisingly well.
    
    Differential Revision: https://phabricator.services.mozilla.com/D176723

--- src/ciadmin/check/check_grants.py
+++ src/ciadmin/check/check_grants.py
@@ -1,10 +1,25 @@
 # This Source Code Form is subject to the terms of the Mozilla Public License,
 # v. 2.0. If a copy of the MPL was not distributed with this file, You can
 # obtain one at http://mozilla.org/MPL/2.0/.
-import pytest
+import re
+from urllib.parse import urlparse
 
-from ciadmin.generate.ciconfig.grants import Grant, ProjectGrantee
+import pytest
+from tcadmin.resources import Resources
+
+from ciadmin.generate.ciconfig.grants import (
+    Grant,
+    ProjectGrantee,
+)
 from ciadmin.generate.ciconfig.projects import Project
+from ciadmin.generate import grants
+
+
+@pytest.fixture(scope="module")
+async def roles():
+    roles = Resources()
+    await grants.update_resources(roles)
+    return roles
 
 
 @pytest.mark.asyncio
@@ -36,3 +51,55 @@ async def check_grant_aliases():
             + "\n".join(sorted(unknown_aliases))
         )
     assert not unknown_aliases
+
+
+@pytest.mark.asyncio
+async def check_insecure_grants(roles):
+    """
+    Ensures we don't grant any level 3 scopes to level 1 contexts.
+    """
+    projects = await Project.fetch_all()
+
+    level_prefixes = {"level", "in-tree-action"}
+    level_prefixes.update({p.trust_domain for p in projects})
+    level_1 = re.compile(f"({'|'.join(level_prefixes)})[-_]1")
+    level_3 = re.compile(f"({'|'.join(level_prefixes)})[-_]3")
+    pr = re.compile(r":pull-request(-untrusted)?$")
+
+    def is_level_1(role):
+        if role.startswith("repo:"):
+            # Check whether the associated project is level 1.
+            repo_url = role.split(":")[1]
+            for project in projects:
+                result = urlparse(project.repo)
+                if repo_url != result.netloc + result.path:
+                    continue
+
+                if project.access == "scm_level_1":
+                    return True
+
+                if project.level == 1:
+                    return True
+
+            # Check whether the role corresponds to a pull request.
+            if pr.search(role):
+                return True
+
+        # Fallback to whether the level-1 regex matches.
+        return bool(level_1.search(role))
+
+    insecure_scopes = set()
+    for role in roles:
+        if not is_level_1(role.roleId):
+            continue
+
+        level_3_scopes = {s for s in role.scopes if level_3.search(s)}
+        if level_3_scopes:
+            insecure_scopes.update(level_3_scopes)
+
+    if insecure_scopes:
+        print(
+            "Level 3 scopes are granted to level 1 contexts:\n  "
+            + "\n  ".join(sorted(insecure_scopes))
+        )
+    assert not insecure_scopes
