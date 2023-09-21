3bc4417962c5abf121a5e50d4d600fe58ba70e3a
Andrew Halberstadt <ahal@mozilla.com>
2023-09-21 14:37:24 +0000

    Bug 1853931 - Rename 'profiler' repo to 'firefox-profiler', r=releng-reviewers,jcristau
    The devtools team wanted to use `firefox-profiler` instead and I agree this is
    more descriptive.
    
    Depends on D188832
    
    Differential Revision: https://phabricator.services.mozilla.com/D188835

--- src/ciadmin/check/check_pull_request_policies.py
+++ src/ciadmin/check/check_pull_request_policies.py
@@ -30,7 +30,7 @@ async def check_pull_request_policies_for_git_repos():
     """
     skip = (
         "occ",  # tc.yml v0
-        "profiler",  # not landed yet
+        "firefox-profiler",  # not landed yet
     )
 
     projects = await Project.fetch_all()
