7e36947d26e03877e22237ea44d95e940a8a0909
Ben Hearsum <ben@mozilla.com>
2024-02-12 17:08:31 +0000

    Bug 1879273: add support for generating templatized clients r=jlorenzo
    Differential Revision: https://phabricator.services.mozilla.com/D201064

---
+++ src/ciadmin/generate/ciconfig/clients_interpreted.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+# This Source Code Form is subject to the terms of the Mozilla Public License,
+# v. 2.0. If a copy of the MPL was not distributed with this file, You can
+# obtain one at http://mozilla.org/MPL/2.0/.
+
+import attr
+
+from ...util.matching import ProjectGrantee, grantees
+from .get import get_ciconfig_file
+
+
+@attr.s(frozen=True)
+class Client:
+    client_id = attr.ib(type=str)
+    scopes = attr.ib(type=list)
+    description = attr.ib(type=str)
+    grantee = attr.ib(type=ProjectGrantee)
+    environments = attr.ib(type=list, default=None)
+
+    @staticmethod
+    async def fetch_all():
+        """Load hook metadata from hooks.yml in ci-configuration"""
+        clients = []
+
+        for client_entry in await get_ciconfig_file("clients-interpreted.yml"):
+            for client_id, info in client_entry["client"].items():
+                for grantee in grantees(client_entry["for"]):
+                    clients.append(
+                        Client(
+                            client_id=client_id,
+                            scopes=info["scopes"],
+                            description=info["description"],
+                            grantee=grantee,
+                            environments=client_entry.get("environments"),
+                        )
+                    )
+
+        return clients
--- src/ciadmin/generate/clients.py
+++ src/ciadmin/generate/clients.py
@@ -7,7 +7,10 @@
 from tcadmin.resources import Client
 
 from .ciconfig.clients import Client as ClientConfig
+from .ciconfig.clients_interpreted import Client as InterpretedClientConfig
 from .ciconfig.environment import Environment
+from .ciconfig.projects import Project
+from .grants import project_match
 
 
 async def update_resources(resources):
@@ -15,6 +18,8 @@ async def update_resources(resources):
     Manage the hooks and roles for cron tasks
     """
     clients = await ClientConfig.fetch_all()
+    interpreted_clients = await InterpretedClientConfig.fetch_all()
+    projects = await Project.fetch_all()
     environment = await Environment.current()
 
     resources.manage("Client=(?!mozilla-auth0/|static/taskcluster/)")
@@ -30,3 +35,21 @@ async def update_resources(resources):
                 scopes=client.scopes,
             )
         )
+
+    for client in interpreted_clients:
+        if client.environments and environment.name not in client.environments:
+            # skip grant for this environment
+            continue
+
+        clients = []
+
+        for project in projects:
+            if project_match(client.grantee, project):
+                subs = {"trust_domain": project.trust_domain}
+                resources.add(
+                    Client(
+                        clientId=client.client_id.format(**subs),
+                        description=client.description.format(**subs),
+                        scopes=[s.format(**subs) for s in client.scopes],
+                    )
+                )
