831f83686bc9cc78cf4a98223b314fd6320edf07
Andrew Halberstadt <ahal@pm.me>
2024-08-02 14:23:16 -0400

    chore: drop support for Python 3.9
    Previously the Jenkins job was using Python 3.9, thus why we were
    running tests there. Now that we're using Github Actions to deploy, and
    the repo is doing likewise, we should test against 3.11 instead.

--- maintenance/pin.sh
+++ maintenance/pin.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 # Pin all requirements.
 #
-# As of this writing, cloudops-jenkins uses an image based off of python:3.9
-# to run ci-admin
-#
 # To test, after running maintenance/pin.sh:
 #
 #    docker pull gcr.io/moz-fx-cloudops-images-global/cloudops-infra-deploylib:2
@@ -16,5 +13,5 @@
 set -e
 set -x
 
-docker run --rm -ti -v $PWD:/src -w /src python:3.9 maintenance/pin-helper.sh
+docker run --rm -ti -v $PWD:/src -w /src python:3.11 maintenance/pin-helper.sh
 docker run --rm -ti -v $PWD:/src -w /src/build-decision python:3.11 ../maintenance/pin-helper.sh
