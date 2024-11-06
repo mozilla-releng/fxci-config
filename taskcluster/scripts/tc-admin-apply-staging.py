5a912ee090dfa130c20297eef1b110abbb277cf1
Andrew Halberstadt <ahal@mozilla.com>
2024-11-06 14:53:14 -0500

    ci: authenticate as Github app in tc-admin-apply-staging
    This will avoid Github rate limits while applying.

--- taskcluster/scripts/tc-admin-apply-staging.py
+++ taskcluster/scripts/tc-admin-apply-staging.py
@@ -13,12 +13,19 @@ CLIENT_ID = "project/releng/fxci-config/apply"
 assert "TASKCLUSTER_PROXY_URL" in os.environ
 options = {"rootUrl": os.environ["TASKCLUSTER_PROXY_URL"]}
 auth = taskcluster.Auth(options)
+secrets = taskcluster.Secrets(options)
 
 result = auth.resetAccessToken(CLIENT_ID)
 assert result
 
 token = result["accessToken"]
 assert isinstance(token, str)
+
+result = secrets.get("project/releng/tc-github-private-key")
+assert isinstance(result, dict)
+
+github_app_creds = result["secret"]
+assert isinstance(github_app_creds, dict)
 
 env = os.environ.copy()
 del env["TASKCLUSTER_PROXY_URL"]  # ensure we don't use the proxy client
@@ -27,6 +34,8 @@ env.update(
         "TASKCLUSTER_ROOT_URL": ROOT_URL,
         "TASKCLUSTER_CLIENT_ID": CLIENT_ID,
         "TASKCLUSTER_ACCESS_TOKEN": token,
+        "GITHUB_APP_ID": str(github_app_creds["app_id"]),
+        "GITHUB_APP_PRIVKEY": github_app_creds["ssh_privkey"],
     }
 )
 subprocess.run(["tc-admin", "apply", "--environment=staging"], env=env, check=True)
