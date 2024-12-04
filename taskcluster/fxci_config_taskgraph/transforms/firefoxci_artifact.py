9c36262e6df9614f5296768f3f5f00c28bb4b154
Andrew Halberstadt <ahal@mozilla.com>
2024-12-04 10:11:47 -0500

    ci: support private artifacts in fxci integration tests

--- taskcluster/fxci_config_taskgraph/transforms/firefoxci_artifact.py
+++ taskcluster/fxci_config_taskgraph/transforms/firefoxci_artifact.py
@@ -2,7 +2,9 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import json
 import os
+from collections import defaultdict
 from copy import deepcopy
 
 from taskgraph.transforms.base import TransformSequence
@@ -35,29 +37,42 @@ def make_firefoxci_artifact_tasks(config, tasks):
     if os.environ["TASKCLUSTER_ROOT_URL"] != STAGING_ROOT_URL:
         return
 
-    found_task_ids = set()
     for task in tasks:
-        fetch_artifacts = task.pop("fetch-artifacts")
-
+        # Format: { <task id>: [<artifact name>]}
+        tasks_to_create = defaultdict(list)
         for decision_index_path in task.pop("decision-index-paths"):
             for task_def in find_tasks(decision_index_path):
-                if "image" not in task_def["payload"]:
-                    continue
+                # Add docker images
+                if "image" in task_def["payload"]:
+                    image = task_def["payload"]["image"]
+                    if not isinstance(image, dict) or "taskId" not in image:
+                        continue
 
-                image = task_def["payload"]["image"]
-                if not isinstance(image, dict) or "taskId" not in image:
-                    continue
+                    task_id = image["taskId"]
+                    if task_id in tasks_to_create:
+                        continue
 
-                task_id = image["taskId"]
-                if task_id in found_task_ids:
-                    continue
+                    tasks_to_create[task_id] = ["public/image.tar.zst"]
 
-                found_task_ids.add(task_id)
+                # Add private artifacts
+                if "MOZ_FETCHES" in task_def["payload"].get("env", {}):
+                    fetches = json.loads(
+                        task_def["payload"].get("env", {}).get("MOZ_FETCHES", {})
+                    )
+                    for fetch in fetches:
+                        if fetch["artifact"].startswith("public"):
+                            continue
 
-                new_task = deepcopy(task)
-                new_task["label"] = f"firefoxci-artifact-{task_id}"
-                new_task["worker"]["env"]["FETCH_URLS"] = get_artifact_urls(
-                    task_id, fetch_artifacts
-                )
+                        task_id = fetch["task"]
+                        tasks_to_create[task_id].append(fetch["artifact"])
 
-                yield new_task
+        for task_id, artifacts in tasks_to_create.items():
+            new_task = deepcopy(task)
+            new_task["label"] = f"firefoxci-artifact-{task['name']}-{task_id}"
+
+            env = new_task["worker"]["env"]
+            env.setdefault("FETCH_URLS", "")
+            env["FETCH_URLS"] = (
+                f"{env['FETCH_URLS']} {get_artifact_urls(task_id, artifacts)}"
+            ).strip()
+            yield new_task
