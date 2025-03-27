08a4260cb41f29d54d45a83791a812ebc1301006
Yaraslau Kurmyza <yarik@mozilla.com>
2025-03-27 13:39:19 +0100

    Allow overrides for maxCapacity and initialWeight per launch config
    Worker pool's top level `worker-manager-config` allows specifying
    `maxCapacity` and `initialWeight` with `by-xx` functionality,
    which makes it possible to set values based on region/zone/machine type.
    
    If `worker-manager-config` is given for `vmSizes` (Azure) or
    `instance_types` (GCP), it will be passed as is to the resulting config.

--- src/ciadmin/generate/worker_pools.py
+++ src/ciadmin/generate/worker_pools.py
@@ -143,6 +143,14 @@ def get_aws_provider_config(
                     instance_type["instanceType"],
                 ):
                     continue
+
+                attrs = {"availabilityZone": availability_zone, "instanceType": instance_type["instanceType"]}
+                initial_weight = evaluate_keyed_by(
+                    worker_manager_config.get("initialWeight", None), "initialWeight", attrs
+                )
+                max_capacity = evaluate_keyed_by(
+                    worker_manager_config.get("maxCapacity", None), "maxCapacity", attrs,
+                )
                 instance_worker_manager_config = merge(
                     {
                         "capacityPerInstance": instance_type.get(
@@ -150,6 +158,8 @@ def get_aws_provider_config(
                         )
                     },
                     worker_manager_config,
+                    {"initialWeight": initial_weight} if initial_weight is not None else {},
+                    {"maxCapacity": max_capacity} if max_capacity is not None else {},
                     instance_type.get("worker-manager-config", {}),
                 )
                 if implementation == "docker-worker":
@@ -301,10 +311,20 @@ def get_azure_provider_config(
                 )
             tags["deploymentId"] = DeploymentId
 
+            attrs = {"location": location, "vmSize": vmSize.get("vmSize")}
+            initial_weight = evaluate_keyed_by(
+                worker_manager_config.get("initialWeight", None), "initialWeight", attrs
+            )
+            max_capacity = evaluate_keyed_by(
+                worker_manager_config.get("maxCapacity", None), "maxCapacity", attrs,
+            )
+
             instance_worker_manager_config = merge(
                 {"capacityPerInstance": vmSize.get("capacityPerInstance", 1)},
                 worker_manager_config,
-                vmSize.get("worker-manager-config", {}),
+                {"initialWeight": initial_weight} if initial_weight is not None else {},
+                {"maxCapacity": max_capacity} if max_capacity is not None else {},
+                vmSize.get("worker-manager-config", {})
             )
 
             launch_config = {
@@ -385,6 +405,14 @@ def get_google_provider_config(
                 ):
                     continue
                 launch_config = copy.deepcopy(instance_type)
+
+                attrs = {"region": region, "zone": zone, "machineType": instance_type["machine_type"]}
+                initial_weight = evaluate_keyed_by(
+                    worker_manager_config.get("initialWeight", None), "initialWeight", attrs,
+                )
+                max_capacity = evaluate_keyed_by(
+                    worker_manager_config.get("maxCapacity", None), "maxCapacity", attrs,
+                )
                 instance_worker_manager_config = merge(
                     {
                         "capacityPerInstance": launch_config.pop(
@@ -392,6 +420,8 @@ def get_google_provider_config(
                         )
                     },
                     worker_manager_config,
+                    {"initialWeight": initial_weight} if initial_weight is not None else {},
+                    {"maxCapacity": max_capacity} if max_capacity is not None else {},
                     launch_config.pop("worker-manager-config", {}),
                 )
                 launch_config["workerManager"] = instance_worker_manager_config
