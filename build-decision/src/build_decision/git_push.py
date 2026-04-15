84ac1be0a80e46fe9716b69c5c8123200e705694
Andrew Halberstadt <ahal@mozilla.com>
2026-04-15 14:43:25 -0400

    fix: add 'as_slugid' to 'git-push' context
    This is context that Taskcluster Github provides to support creating new
    slugids withint JSON-e. We should provide a similar feature in
    `build-decision` so we don't need to handle the two cases separately
    within .taskcluster.yml
    
    Bug: 2030733

--- build-decision/src/build_decision/git_push.py
+++ build-decision/src/build_decision/git_push.py
@@ -6,6 +6,8 @@ import json
 import logging
 import os
 
+import slugid
+
 from .decision import render_tc_yml
 
 logger = logging.getLogger(__name__)
@@ -33,11 +35,19 @@ def build_decision(*, repository, dry_run):
 
     tc_yml = repository.get_file(".taskcluster.yml", revision=event["after"])
 
+    _slugids = {}
+
+    def as_slugid(name):
+        if name not in _slugids:
+            _slugids[name] = slugid.nice()
+        return _slugids[name]
+
     task = render_tc_yml(
         tc_yml,
         taskcluster_root_url=os.environ["TASKCLUSTER_ROOT_URL"],
         tasks_for="git-push",
         event=event,
+        as_slugid=as_slugid,
     )
 
     task.display()
