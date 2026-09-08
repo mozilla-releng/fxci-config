6609fbe29294b8b348b796104ec11274306c653b
Jonathan Moss <jmoss@mozilla.com>
2026-09-08 11:42:31 -0400

    RELOPS-2548: simplify FooFrix pool change

--- src/ciadmin/generate/worker_pools.py
+++ src/ciadmin/generate/worker_pools.py
@@ -189,9 +189,7 @@ def _build_arm_template_launch_config(
             "imageId": image_reference_id,
             "location": loc,
             "subnetId": subnet_id,
-            "priority": pool_cfg.get(
-                "priority", "Spot" if pool_cfg.get("spot", True) else "Regular"
-            ),
+            "priority": pool_cfg.get("priority", "Spot"),
         }
     )
 
@@ -785,7 +783,6 @@ def generate_pool_variants(worker_pools, environment):
             "regions",
             "scalingRatio",
             "security",
-            "spot",
             "tags.sourceBranch",
             "vmSizes.launchConfig.hardwareProfile.vmSize",
             "worker-purpose",
